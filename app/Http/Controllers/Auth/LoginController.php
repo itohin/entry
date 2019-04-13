02b07a8a5379aa27d51865a1fc060460e039cd6b
Anton Tomin <iToha@iMac-Anton.local>
2019-04-13 23:44:04 +0300

    Login validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,33 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => [
+                    'required', 'string',
+                    Rule::exists('users')->where(function ($query) {
+                        $query->where('active', true);
+                    })
+                ],
+            'password' => 'required|string',
+        ], $this->validationErrors());
+    }
+
+    protected function validationErrors()
+    {
+        return [
+            $this->username() . '.exists' => 'No account found, or you need to activate your account'
+        ];
     }
 }
